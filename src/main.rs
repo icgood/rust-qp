7946ac184fd9af30dbc08d467dd0b9af1a52036f
Ian Good <ian.good@rackspace.com>
2014-11-30 22:20:12 -0500

    read from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 extern crate qp;
+use std::io::stdio::stdin;
 use qp::ToQP;
 
 fn main() {
-    let s = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt \r\nin culpa qui officia deserunt mollit anim id est laborum.";
+    let s = stdin().read_to_end().unwrap();
     println!("{}", s.as_bytes().to_qp(Some(76)));
 }
