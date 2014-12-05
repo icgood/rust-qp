fadd5672311be67a5a81d0e71c2f2f08e70ff2c0
Ian Good <ian.good@rackspace.com>
2014-12-04 21:07:33 -0500

    getting faster

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,7 @@ const SPACE: u8 = 32;
 const TAB: u8 = 9;
 
 /// A trait for converting a value to quoted-printable encoding.
-pub trait ToQP for Sized? {
+pub trait ToQP {
     /// Converts the value of `self` to a quoted-printable value following the specified format
     /// configuration, returning the owned string.
     fn to_qp(&self, line_length: Option<uint>) -> String;
@@ -36,13 +36,13 @@ fn check_width(line_length: &Option<uint>, trk: &mut Tracking, change: uint) {
     match *line_length {
         Some(len) =>
             if trk.width + change >= len {
-                push_encoded(line_length, trk, vec![EQ]);
+                push_encoded(line_length, trk, &[EQ]);
             },
         None => ()
     }
 }
 
-fn push_encoded(line_length: &Option<uint>, trk: &mut Tracking, encoded: Vec<u8>) {
+fn push_encoded(line_length: &Option<uint>, trk: &mut Tracking, encoded: &[u8]) {
     if encoded[0] == EQ && encoded.len() == 1 {
         // Want to push a soft newline.
         trk.buf.push_all(trk.white.as_slice());
@@ -57,7 +57,7 @@ fn push_encoded(line_length: &Option<uint>, trk: &mut Tracking, encoded: Vec<u8>
         LF => {
             // Want to push a hard newline.
             if trk.white.len() > 0 {
-                push_encoded(line_length, trk, vec![EQ]);
+                push_encoded(line_length, trk, &[EQ]);
             }
             trk.buf.push_all(&[CR, LF]);
             trk.width = 0;
@@ -81,6 +81,31 @@ fn push_encoded(line_length: &Option<uint>, trk: &mut Tracking, encoded: Vec<u8>
     }
 }
 
+fn encode_byte(line_length: &Option<uint>, trk: &mut Tracking, b1: u8, b2: u8) {
+    match (b1, b2) {
+        (CR, LF) => {
+            push_encoded(&line_length, &mut trk, &[LF]);
+            iter.next();
+        }
+        _ => {
+            match b1 {
+                LF => {
+                    push_encoded(&line_length, &mut trk, &[LF]);
+                },
+                9 | 32 ... 60 | 62 ... 126 => {
+                    push_encoded(&line_length, &mut trk, &[b1]);
+                },
+                _ => {
+                    let encoded = &[61,
+                                    HEX_CHARS[(b1 as uint >> 4)],
+                                    HEX_CHARS[(b1 as uint & 0xf)]];
+                    push_encoded(&line_length, &mut trk, encoded);
+                }
+            }
+        }
+    }
+}
+
 impl ToQP for [u8] {
     /// Turn a vector of `u8` bytes into a quoted-printable string.
     ///
@@ -95,66 +120,24 @@ impl ToQP for [u8] {
     /// }
     /// ```
     fn to_qp(&self, line_length: Option<uint>) -> String {
-        let iter2 = self.iter().skip(2).fuse();
-        let iter1 = self.iter().skip(1).fuse();
-        let mut line = Vec::with_capacity(line_length.unwrap_or(76));
-        let mut v = Vec::with_capacity(self.len());
+        let mut tracking = Tracking { buf: Vec::with_capacity(self.len()*3),
+                                      white: Vec::with_capacity(76),
+                                      width: 0 };
+        let mut iter = self.iter().peekable();
 
-        let encode_char = |c: u8| {
-            &[EQ,
-              HEX_CHARS[(c as uint >> 4)],
-              HEX_CHARS[(c as uint & 0xf)]];
-        };
-
-            let next_one = iter1.next();
-            let next_two = (next_one, iter2.next());
-    }
-
+        loop {
+            let b1 = match iter.next() {
+                Some(c) => *c,
+                None => break,
+            };
+            let b2 = match iter.peek() {
+                Some(c) => **c,
+                None => 0,
+            };
+            encode_byte(&line_length, &mut tracking, b1, b2);
+        }
         unsafe {
-            String::from_utf8_unchecked(v)
+            String::from_utf8_unchecked(tracking.buf)
         }
     }
-    //fn to_qp(&self, line_length: Option<uint>) -> String {
-    //    let mut tracking = Tracking { buf: Vec::new(), white: Vec::new(), width: 0 };
-    //    let mut i = 0;
-    //    let len = self.len();
-
-    //    while i < len {
-    //        let b1 = self[i];
-    //        let b2 = if i + 1 < len {
-    //            self[i + 1]
-    //        }
-    //        else {
-    //            0
-    //        };
-
-    //        match (b1, b2) {
-    //            (CR, LF) => {
-    //                push_encoded(&line_length, &mut tracking, vec![LF]);
-    //                i += 2;
-    //            }
-    //            _ => {
-    //                match b1 {
-    //                    LF => {
-    //                        push_encoded(&line_length, &mut tracking, vec![LF]);
-    //                    },
-    //                    9 | 32 ... 60 | 62 ... 126 => {
-    //                        push_encoded(&line_length, &mut tracking, vec![b1]);
-    //                    },
-    //                    _ => {
-    //                        let encoded = vec![61,
-    //                                           HEX_CHARS[(b1 as uint >> 4)],
-    //                                           HEX_CHARS[(b1 as uint & 0xf)]];
-    //                        push_encoded(&line_length, &mut tracking, encoded);
-    //                    }
-    //                }
-    //                i += 1;
-    //            }
-    //        }
-
-    //    }
-    //    unsafe {
-    //        String::from_utf8_unchecked(tracking.buf)
-    //    }
-    //}
 }
